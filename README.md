a266104d818f7306f4007520d3b352841823a1b9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-17 16:08:34 -0500

    add test for comment-value, use npm test

---
+++ README.md
@@ -0,0 +1,4 @@
+# comment-value-test
+
+Test project for [comment-value](https://github.com/bahmutov/comment-value)
+tool.
---
+++ index.js
@@ -0,0 +1,5 @@
+const add = (a, b) => a + b
+add(1, 2) //> 3
+add(-1, 1) //> 0
+const result = add(1, 1) + add(2, 2)
+result
