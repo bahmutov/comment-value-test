032d46b33a4e0751fdb8f0d929949d43971ca346
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-18 11:55:56 -0500

    updated example

--- expected.js
+++ expected.js
@@ -3,9 +3,11 @@ add(1, 2) //> 3
 add(-1, 1) //> 0
 const result = add(1, 1) + add(2, 2)
 
-// compute 2 * 5 + 3
+// compute x * 5 + 3
 const add3 = x => x + 3
 const mul5 = x => x * 5
-add3(mul5(2)) //> 13
+add3(mul5(0)) //> 3
+add(1, 2) //> 3
+add(1, -2) //> -1
 
 console.log('all done in', __filename)
