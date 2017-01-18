2aeb871b056424f68446640e7d1d1491b94f29f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-18 00:33:13 -0500

    more stuff in index.js

--- expected.js
+++ expected.js
@@ -2,4 +2,8 @@ const add = (a, b) => a + b
 add(1, 2) //> 3
 add(-1, 1) //> 0
 const result = add(1, 1) + add(2, 2)
-result
+
+// compute 2 * 5 + 3
+const add3 = x => x + 3
+const mul5 = x => x * 5
+add3(mul5(2)) //> 13
